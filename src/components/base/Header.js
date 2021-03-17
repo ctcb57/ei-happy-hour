6ebad00d26c0a0cbcd3659e7c491e20ffed6adf8
charles clark <charles@pop-os.localdomain>
2021-03-17 17:50:55 -0500

    Updated logo and favicon

--- src/components/base/Header.js
+++ src/components/base/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
         <>
             <header className="header">
                 <div className="header__logo-box">
-                    <a href="/">Home</a>
+                    <img alt="Logo" className="header__logo" data-uk-img="" src="https://heliumbooks-webapp.s3.us-east-2.amazonaws.com/a6fb3590a9a94e8db581a4b340397689+(1).png"/>
                 </div>
                 <div className="header__text-box">
                     <h1 className="heading-primary">
